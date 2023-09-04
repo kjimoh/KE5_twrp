042a05a5ebdc6b0ea33cd431aa14aeaa0d8c2424
Kamaldeen Jimoh <jimohkamal446@gmail.com>
2023-09-04 11:22:41 +0100

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_TECNO-KE5.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_TECNO-KE5-user \
+    twrp_TECNO-KE5-userdebug \
+    twrp_TECNO-KE5-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for TECNO TECNO KE5 (TECNO-KE5)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
