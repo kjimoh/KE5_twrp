49b5f41d6fa545cfcad08a83c2e3aecc17765853
Kamaldeen Jimoh <jimohkamal446@gmail.com>
2023-09-06 01:31:00 +0100

    update

--- Android.mk
+++ Android.mk
@@ -5,17 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#LOCAL_PATH := $(call my-dir)
-
-#ifeq ($(TARGET_DEVICE),TECNO-KE5)
-#include $(call all-subdir-makefiles,$(LOCAL_PATH))
-#endif
-
-
-ifneq ($(filter TECNO-KE5, $(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+ifeq ($(TARGET_DEVICE),TECNO-KE5)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+
+#ifneq ($(filter TECNO-KE5, $(TARGET_DEVICE)),)
+
+#LOCAL_PATH := $(call my-dir)
+
+#include $(call all-makefiles-under,$(LOCAL_PATH))
+
+#endif
